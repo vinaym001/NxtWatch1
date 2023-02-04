ec65be67659d891da6d5818efc7cf62412012812
vinaym001 <mvinay.vn@gmail.com>
2023-02-04 17:48:42 +0000

    change 1

--- src/App.js
+++ src/App.js
@@ -4,11 +4,17 @@ import {Route} from 'react-router-dom'
 
 import Login from './components/Login'
 import Home from './components/Home'
+import Trending from './components/Trending'
+import Gaming from './components/Gaming'
+import VideoItemDetails from './components/VideoItemDetails'
 
 const App = () => (
   <div>
     <Route path="/login" component={Login} />
     <Route exact path="/" component={Home} />
+    <Route exact path="/trending" component={Trending} />
+    <Route exact path="/gaming" component={Gaming} />
+    <Route exact path="/videos/:id" component={VideoItemDetails} />
   </div>
 )
 
